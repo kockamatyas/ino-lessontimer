2b38a6db6edb98144d8326fbdffcdcb949400a4f
Ondrej Mecl <meclondrej@protonmail.com>
2023-09-09 14:40:09 +0200

    Added the display

--- sketch.ino
+++ sketch.ino
@@ -1,2 +1,12 @@
-void setup() {}
+#include "TM1637Display.h"
+
+#define DISPLAY_CLK 2
+#define DISPLAY_DIO 3
+#define DISPLAY_BRIGHTNESS 7
+
+TM1637Display display(DISPLAY_CLK, DISPLAY_DIO);
+
+void setup() {
+    display.setBrightness(DISPLAY_BRIGHTNESS);
+}
 void loop() {}
